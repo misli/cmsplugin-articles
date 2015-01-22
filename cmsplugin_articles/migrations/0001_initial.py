adde1af69060d7b9ee9dc37e208cc0a350ebcc39
Jakub Dorňák <jdornak@redhat.com>
2015-01-22 01:38:12 +0100

    Django migrations

--- cmsplugin_articles/migrations/0001_initial.py
+++ cmsplugin_articles/migrations/0001_initial.py
@@ -1,71 +1,68 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import cms.models.fields
+import djangocms_text_ckeditor.fields
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Article'
-        db.create_table(u'cmsplugin_articles_article', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('polymorphic_ctype', self.gf('django.db.models.fields.related.ForeignKey')(related_name=u'polymorphic_cmsplugin_articles.article_set', null=True, to=orm['contenttypes.ContentType'])),
-            ('namespace', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=250)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=250)),
-            ('pub_date', self.gf('django.db.models.fields.DateField')()),
-            ('perex', self.gf('djangocms_text_ckeditor.fields.HTMLField')(default=u'', blank=True)),
-            ('text', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['cms.Placeholder'], null=True)),
-            ('page_title', self.gf('django.db.models.fields.CharField')(max_length=250, null=True, blank=True)),
-            ('menu_title', self.gf('django.db.models.fields.CharField')(max_length=250, null=True, blank=True)),
-            ('meta_desc', self.gf('django.db.models.fields.TextField')(default=u'', blank=True)),
-            ('public', self.gf('django.db.models.fields.BooleanField')(default=False)),
-        ))
-        db.send_create_signal(u'cmsplugin_articles', ['Article'])
+    dependencies = [
+        ('cms', '0003_auto_20140926_2347'),
+        ('contenttypes', '0001_initial'),
+    ]
 
-        # Adding unique constraint on 'Article', fields ['pub_date', 'slug']
-        db.create_unique(u'cmsplugin_articles_article', ['pub_date', 'slug'])
-
-
-    def backwards(self, orm):
-        # Removing unique constraint on 'Article', fields ['pub_date', 'slug']
-        db.delete_unique(u'cmsplugin_articles_article', ['pub_date', 'slug'])
-
-        # Deleting model 'Article'
-        db.delete_table(u'cmsplugin_articles_article')
-
-
-    models = {
-        'cms.placeholder': {
-            'Meta': {'object_name': 'Placeholder'},
-            'default_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'slot': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'})
-        },
-        u'cmsplugin_articles.article': {
-            'Meta': {'unique_together': "[(u'pub_date', u'slug')]", 'object_name': 'Article'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'menu_title': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'}),
-            'meta_desc': ('django.db.models.fields.TextField', [], {'default': "u''", 'blank': 'True'}),
-            'namespace': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
-            'page_title': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'}),
-            'perex': ('djangocms_text_ckeditor.fields.HTMLField', [], {'default': "u''", 'blank': 'True'}),
-            'polymorphic_ctype': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'polymorphic_cmsplugin_articles.article_set'", 'null': 'True', 'to': u"orm['contenttypes.ContentType']"}),
-            'pub_date': ('django.db.models.fields.DateField', [], {}),
-            'public': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '250'}),
-            'text': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Placeholder']", 'null': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '250'})
-        },
-        u'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        }
-    }
-
-    complete_apps = ['cmsplugin_articles']
+    operations = [
+        migrations.CreateModel(
+            name='Article',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('namespace', models.CharField(max_length=200, verbose_name='Application instance')),
+                ('title', models.CharField(max_length=250, verbose_name='Title')),
+                ('slug', models.SlugField(max_length=250, verbose_name='Slug')),
+                ('pub_date', models.DateField(verbose_name='Publication date')),
+                ('perex', djangocms_text_ckeditor.fields.HTMLField(default='', verbose_name='Perex', blank=True)),
+                ('page_title', models.CharField(help_text='Overwrite the title (html title tag)', max_length=250, null=True, verbose_name='Page title', blank=True)),
+                ('menu_title', models.CharField(help_text='Overwrite the title in the menu', max_length=250, null=True, verbose_name='Menu title', blank=True)),
+                ('meta_desc', models.TextField(default='', help_text='The text displayed in search engines.', verbose_name='Meta description', blank=True)),
+                ('public', models.BooleanField(default=False, verbose_name='Public')),
+                ('polymorphic_ctype', models.ForeignKey(related_name='polymorphic_cmsplugin_articles.article_set', editable=False, to='contenttypes.ContentType', null=True)),
+                ('text', cms.models.fields.PlaceholderField(slotname='article_text', editable=False, to='cms.Placeholder', null=True)),
+            ],
+            options={
+                'ordering': ('-pub_date',),
+                'verbose_name': 'Article',
+                'verbose_name_plural': 'Articles',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='ArticlePlugin',
+            fields=[
+                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
+                ('template', models.CharField(default='default', help_text='The template used to render plugin.', max_length=100, verbose_name='Template', choices=[('default', 'Default')])),
+                ('article', models.ForeignKey(verbose_name='Article', to='cmsplugin_articles.Article')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('cms.cmsplugin',),
+        ),
+        migrations.CreateModel(
+            name='LastArticlesPlugin',
+            fields=[
+                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
+                ('number', models.IntegerField(default=3, verbose_name='Number of last articles')),
+                ('template', models.CharField(default='default', help_text='The template used to render plugin.', max_length=100, verbose_name='Template', choices=[('default', 'Default')])),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('cms.cmsplugin',),
+        ),
+        migrations.AlterUniqueTogether(
+            name='article',
+            unique_together=set([('pub_date', 'slug')]),
+        ),
+    ]
