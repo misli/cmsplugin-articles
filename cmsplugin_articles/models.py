2287d54ce69199c13512b7c2d7abfb38c345f0d8
Jakub Dorňák <jdornak@redhat.com>
2016-01-03 09:12:46 +0100

    fixed import of PolymorphicModel

--- cmsplugin_articles/models.py
+++ cmsplugin_articles/models.py
@@ -10,7 +10,7 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.functional import cached_property
 from django.utils.translation import ugettext_lazy as _
-from polymorphic.polymorphic_model import PolymorphicModel
+from polymorphic.models import PolymorphicModel
 
 from .utils import get_html_field
 
